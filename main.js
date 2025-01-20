1d1bfb54bb0d290e3f426d488a053b4cac8e9b45
Resti <restiaulia480@gmail.com>
2025-01-20 09:21:04 +0700

    update

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js'
 import {
   getFirestore,
@@ -23,18 +22,19 @@ const firebaseConfig = {
   measurementId: "G-TJFSY9D8R1"
 };
 
-// Inisialisasi firebase
-const aplikasi = initializeApp(firebaseConfig)
-const basisdata = getFirestore(aplikasi)
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const basisdata = getFirestore(app);
 
+// fungsi ambil daftar pelanggan 
 export async function ambilDaftarPelanggan() {
   const refDokumen = collection(basisdata, "pelanggan");
   const kueri = query(refDokumen, orderBy("nama"));
-  const cuplikanKueri = await getDocs(kueri);
+  const cuplikankueri = await getDocs(kueri);
 
-  let hasilKueri = [];
-  cuplikanKueri.forEach((dokumen) => {
-    hasilKueri.push({
+  let hasilkueri = [];
+  cuplikankueri.forEach((dokumen) => {
+    hasilkueri.push({
       id: dokumen.id,
       nama: dokumen.data().nama,
       alamat: dokumen.data().alamat,
@@ -42,7 +42,7 @@ export async function ambilDaftarPelanggan() {
 
     })
   })
-  
+
   return hasilkueri;
 }
 
@@ -57,9 +57,28 @@ export async function tambahPelanggan(nama, alamat, nohape) {
     })
 
     // menampilkan pesan berhasil
-    console.log('berhasil menyimpan data pelanggan')
+    console.log("berhasil menyimpan data pelanggan")
   } catch (error) {
     // menampilkan pesan gagal 
-    console.log('gagal menyimpan data pelanggan' + error)
+    console.log("gagal menyimpan data pelanggan")
   }
+}
+
+export async function hapusPelanggan(id) {
+  await deleteDoc(doc(basisdata, "pelanggan", id))
+}
+
+
+export async function ubahPelanggan(id, namabaru, alamatbaru, nohapebaru) {
+  await updateDoc(
+    doc(basisdata, "pelanggan", id), { nama: namabaru, alamat: alamatbaru, nohape: nohapebaru }
+  )
+
+}
+
+export async function ambilPelanggan(id) {
+  const refDokumen = await doc(basisdata, "pelanggan", id)
+  const snapshotDokumen = await getDoc(refDokumen)
+
+  return await snapshotDokumen.data()
 }
